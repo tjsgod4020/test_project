da76675bbdec498bfc0f757adc38501ffd0b3209
DESKTOP-T0GNUJP\SUN <tjsgod4020@gmail.com>
2018-05-23 00:07:41 +0900

    폴더전체 테스트

---
+++ TEST_UI_4/appUI/src/main/java/com/example/sun/test_ui_4/appUI/OtherActivity.java
@@ -0,0 +1,13 @@
+package com.example.sun.test_ui_4.appUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
